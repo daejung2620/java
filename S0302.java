8bd3edb01391b22ffa373a288a9c9d172b74140b
daejung2620 <daejung2620@dge.go.kr>
2026-03-31 10:30:17 +0900

    Update S0302.java

--- S0302.java
+++ S0302.java
@@ -1,8 +1,14 @@
 package jump2java;
 
-public class S0302 {
+public class S0301 {
 	public static void main(String[] args) {
 		System.out.println("Hello, java");
+		int i = 0;
+		System.out.println(i++);
+		System.out.println(i);
+		System.out.println(++i);
+		System.out.println(i);
+		
 		
 	}
 }
