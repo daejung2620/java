4a921145c1d9d8a1862d7fc7d7c50a05f9b0e661
daejung2620 <daejung2620@dge.go.kr>
2026-03-31 10:30:55 +0900

    Update S0303.java

--- S0303.java
+++ S0303.java
@@ -1,8 +1,11 @@
 package jump2java;
 
-public class S0303 {
+public class S0302 {
 	public static void main(String[] args) {
-		System.out.println("Hello, java");
-		
+		String a = "Hello java";
+		System.out.println(a.contains("java"));
+		System.out.println(a.charAt(5));
+		System.out.println(a.replaceAll("java","world"));
+		System.out.println(a.substring(3,8));
 	}
 }
