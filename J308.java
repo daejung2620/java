17dc31ee323138eee192c94da1810270e75fa8ec
daejung2620 <daejung2620@dge.go.kr>
2026-04-14 09:37:16 +0900

    Add files via upload

---
+++ J308.java
@@ -0,0 +1,10 @@
+package jump2java;
+import java.util.HashMap;
+
+public class J308 {
+	public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("people", "사람");
+        map.put("baseball", "야구");
+	}
+}
