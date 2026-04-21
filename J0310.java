2660f1ea0ea9dbc14640f0ee7b1886a47f104a0b
daejung2620 <daejung2620@dge.go.kr>
2026-04-21 10:37:36 +0900

    Add files via upload

---
+++ J0310.java
@@ -0,0 +1,16 @@
+package jump2java;
+
+public class SampleJ0310 {
+    enum CoffeeType {
+        AMERICANO,
+        ICE_AMERICANO,
+        CAFE_LATTE
+    };
+
+    public static void main(String[] args) {
+        System.out.println(CoffeeType.AMERICANO);
+        System.out.println(CoffeeType.ICE_AMERICANO);
+        System.out.println(CoffeeType.CAFE_LATTE);
+    }
+}
+
---
+++ J0401.java
@@ -0,0 +1,12 @@
+package jump2java;
+
+public class J0401 {
+	public static void main(String[] args) {
+		int money = 2000;
+		if (money >= 3000) {
+			System.out.println("택시를 타고 가라");
+		}else {
+			System.out.println("걸어가라");
+		}
+	}
+}
