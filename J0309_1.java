bb3fdce360163cf87c9002fce8858cbba1a85fa9
daejung2620 <daejung2620@dge.go.kr>
2026-04-21 10:10:09 +0900

    Add files via upload

---
+++ J0309_1.java
@@ -0,0 +1,10 @@
+package jump2java;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class J0309_1 {
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<>(Arrays.asList("H","E","L","L","O"));
+		System.out.println(set);
+	}
+}
